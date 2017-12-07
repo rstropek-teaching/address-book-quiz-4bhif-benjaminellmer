684e19c86683c769cf2395b7316b71e7245cd9e1
DESKTOP-KR9H572\Benjamin <benjamin.ellmer@yahoo.com>
2017-12-07 15:23:01 +0100

    moved the code into the src folder

---
+++ src/data.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.customers = [
+    { id: 1, firstName: 'Benjamin', lastName: 'Ellmer', email: "benjamin.ellmer@yahoo.com" },
+    { id: 2, firstName: 'Simon', lastName: 'Straßer', email: "simon.straßer@gmail.com" },
+    { id: 3, firstName: 'Alex', lastName: 'ROB', email: "alex.rob@gmx.net" },
+    { id: 4, firstName: 'Fabian', lastName: 'Lange', email: "handy.lange@gmail.com" },
+    { id: 5, firstName: 'Jakob', lastName: 'Strasser', email: "jakob.strasser@yahoo.com" },
+    { id: 6, firstName: 'Philip', lastName: 'Diesenreiter', email: "philip.diesenreiter@gmail.com" },
+    { id: 7, firstName: 'Christian', lastName: 'Weber', email: "chreber.wistian@yahoo.com" },
+    { id: 8, firstName: 'Haniel', lastName: 'Dummel', email: "haniel.dummel@gmx.net" }
+];
---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export interface ICustomer {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+  }
+  
+export const customers: ICustomer[] = [
+    {id: 1, firstName: 'Benjamin', lastName: 'Ellmer', email: "benjamin.ellmer@yahoo.com"},
+    {id: 2, firstName: 'Simon', lastName: 'Straßer',email: "simon.straßer@gmail.com"},
+    {id: 3, firstName: 'Alex', lastName: 'ROB',email: "alex.rob@gmx.net"},
+    {id: 4, firstName: 'Fabian', lastName: 'Lange',email: "handy.lange@gmail.com"},
+    {id: 5, firstName: 'Jakob', lastName: 'Strasser',email: "jakob.strasser@yahoo.com"},
+    {id: 6, firstName: 'Philip', lastName: 'Diesenreiter',email: "philip.diesenreiter@gmail.com"},
+    {id: 7, firstName: 'Christian', lastName: 'Weber',email: "chreber.wistian@yahoo.com"},
+    {id: 8, firstName: 'Haniel', lastName: 'Dummel',email: "haniel.dummel@gmx.net"}
+  ];
